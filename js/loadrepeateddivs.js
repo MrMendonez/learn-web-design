154b3c6f553ac2b02fbbd32d8698eb34b9f82f34
MrMendonez <dmendonez@gmail.com>
2016-02-01 13:48:55 -0500

    moving learnwebdesign from google drive to github and heroku

---
+++ js/loadrepeateddivs.js
@@ -0,0 +1,11 @@
+/* Learn Web Design */
+
+function loadRepeatedDivs(){
+
+    $("#navigation-bar").load("js/loadnavigationbar.html");
+
+    $("#contact-pop-up").load("js/loadcontactpopup.html");
+
+}
+
+$(document).ready(function() { loadRepeatedDivs(); });
